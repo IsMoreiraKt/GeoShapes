3d2d7ba27e3135f30419557a4bc9a1938eb1666e
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 04:58:08 +0000

    feat: Added screen clearing when exiting the program.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import os
+
+
+
 def main():
     exit_program = False
 
@@ -8,10 +12,16 @@ def main():
 
         try:
             choice = int(input("0 -> Exit\n1 -> Circle\n2 -> Square\n3 -> Triangle\n\t-> "))
+        
+            if (choice == 0):
+                exit_program == True
         except TypeError:
             print("Please enter numbers only.")
+
+        print("\n\n\n")
 
 
 
 if __name__ == '__main__':
     main()
+    os.system('cls' if os.name == 'nt' else 'clear')
