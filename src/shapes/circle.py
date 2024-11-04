450165850da1b3e33e128c5c71dc069ec9aa5744
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 04:37:49 +0000

    feat: Addition of the method that calculates the area in the circle class.

--- src/shapes/circle.py
+++ src/shapes/circle.py
@@ -1,6 +1,12 @@
 from shapes.shape import Shape
+import math
+
 
 
 class Circle(Shape):
     def __init__(self, radius: int) -> None:
         self.radius = radius
+
+
+    def calculate_area(self) -> float:
+        return math.pi * (self.radius ** 2)
