4e52e8ce8f3436f9601c1434d008b127a14c6ddc
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 04:34:05 +0000

    feat: Addition of the perimeter calculation method to the shape class.

--- src/shapes/shape.py
+++ src/shapes/shape.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 
 
+
 class Shape(ABC):
     @abstractmethod
-    def calculate_area(self) -> None:
+    def calculate_area(self) -> float:
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self) -> float:
         pass
