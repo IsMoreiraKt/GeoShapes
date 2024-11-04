136d0f5d2460d49c438a008d2da29ebb9dbbf084
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 04:56:08 +0000

    feat: Adding a loop to the program to keep it running.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
 def main():
+    exit_program = False
+
     print("----| Welcome to GeoShapes! |----")
+
+    while not exit_program:
+        print("Which picture do you want to use for the test?")
+
+        try:
+            choice = int(input("0 -> Exit\n1 -> Circle\n2 -> Square\n3 -> Triangle\n\t-> "))
+        except TypeError:
+            print("Please enter numbers only.")
 
 
 
