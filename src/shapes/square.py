9368a9af1a5eb100f40ca9b4a2d8773447a503ef
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 04:40:39 +0000

    feat: Addition of the method that calculates the area in the square class.

--- src/shapes/square.py
+++ src/shapes/square.py
@@ -5,3 +5,7 @@ from shapes.shape import Shape
 class Square(Shape):
     def __init__(self, side: float) -> None:
         self.side = side
+
+
+    def calculate_area(self):
+        return self.side ** 2
