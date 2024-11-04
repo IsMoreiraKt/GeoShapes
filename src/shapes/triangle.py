8f051ff4cfc8a7fe41d96710edc3e12ad67b8375
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 04:44:24 +0000

    feat: Creation of the triangle class.

---
+++ src/shapes/triangle.py
@@ -0,0 +1,13 @@
+from shapes.shape import Shape
+
+
+
+class Triangle(Shape):
+    def __init__(
+        self, base: float, height: float, 
+        first_side: float, second_side: float
+    ) -> None:
+        self.base = base
+        self.height = height
+        self.first_side = first_side
+        self.second_side = second_side
