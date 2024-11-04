3523248ab53d2a9fb220da1e1e32ea37b9f99fa8
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 05:18:31 +0000

    fix: Correction of the program output check.

--- src/main.py
+++ src/main.py
@@ -100,7 +100,7 @@ def main() -> None:
             choice = int(input("0 -> Exit\n1 -> Circle\n2 -> Square\n3 -> Triangle\n\t-> "))
         
             if (choice == 0):
-                exit_program == True
+                exit_program = True
             elif (choice == 1):
                 circle_handler()
             elif (choice == 2):
